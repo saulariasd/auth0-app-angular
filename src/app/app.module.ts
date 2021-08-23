cc042ac467b719ee150628624636ff03e13e9343
saulariasd <saul_ariasd@hotmail.com>
2021-08-23 18:44:46 -0500

    login with Auth0

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
+
+import { AuthModule } from '@auth0/auth0-angular';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PreciosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    // Import the module into the application, with configuration
+    AuthModule.forRoot({
+      domain: 'dev-gdzcfwgq.us.auth0.com',
+      clientId: 'TMOVQK5MDa4It44WBD87vW3JZw4QhJAU'
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
